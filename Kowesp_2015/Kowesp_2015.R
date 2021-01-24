5485a6ea18864b424f2ca743ce58762c7e9aa70a
lee-online <77882113+lee-online@users.noreply.github.com>
2021-01-24 21:44:12 +0900

    Add files via upload

--- Kowesp_2015/Kowesp_2015.R
+++ Kowesp_2015/Kowesp_2015.R
@@ -5,7 +5,24 @@ library(ggplot2)
 library(readxl)
 
 
-#load data
 
-raw_data = read.spss(file="C:/Users/indib/Desktop/R-REMINDER/R-reminder/Koweps_2015/Koweps_2015.sav", to.data.frame = T)
-df
+raw_data = read.spss(file="Koweps_2015.sav", to.data.frame = T)#load data
+data = raw_data #copy
+
+#overview
+head(data)
+View(data)
+dim(data)
+str(data)
+
+#machining
+data = rename(data, sex = h10_g3, birth = h10_g4, marriage = h10_g10, religion = h10_g11, income = p1002_8aq1, job = h10_eco9, region = h10_reg7)
+data = select(data, sex, birth, marriage, religion, income, job, region)
+table(is.na(data))
+View(data)
+
+data_new = na.omit(data)
+View(data_new)
+summary(data_new)
+
+summary(data_new$income)
